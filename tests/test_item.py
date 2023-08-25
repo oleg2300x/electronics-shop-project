26421ab9b7cf91b0bdd1004fe67cbcd739e176e8
Oleg <panasenko23o@mail.ru>
2023-08-25 23:48:41 +1000

    append test homework2

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,12 +2,44 @@
 
 from src.item import Item
 
-def test_calculate_total_price():
+import pytest
+
+
+@pytest.fixture()
+def test_file():
     emp1 = Item('iPhone', 100000, 5)
-    assert emp1.calculate_total_price() == 500000
+    return emp1
 
-def test_apply_discount():
-    emp2 = Item('Watch', 10000, 2)
+
+def test_calculate_total_price(test_file):
+    assert test_file.calculate_total_price() == 500000
+
+
+def test_apply_discount(test_file):
     Item.pay_rate = 0.5
-    assert emp2.apply_discount() == 5000
+    assert test_file.apply_discount() == 50000
 
+
+def test_name(test_file):
+    assert test_file.name == 'iPhone'
+
+
+def test_set_name(test_file):
+    test_file.name = 'Pokemon'
+    assert test_file.name == 'Pokemon'
+
+    test_file.name = 'incomprehensibility'
+    assert test_file.name == 'incomprehe'
+
+
+def test_instantiate_from_csv():
+    Item.all = []
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number(test_file):
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('6.9') == 6
+    assert Item.string_to_number(553.654) == 553
+    assert Item.string_to_number(92134.0) == 92134
