7ac727e623cf06a86869dea730bd97949f03ba81
Oleg <panasenko23o@mail.ru>
2023-08-25 16:18:38 +1000

    append test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+def test_calculate_total_price():
+    emp1 = Item('iPhone', 100000, 5)
+    assert emp1.calculate_total_price() == 500000
+
+def test_apply_discount():
+    emp2 = Item('Watch', 10000, 2)
+    Item.pay_rate = 0.5
+    assert emp2.apply_discount() == 5000
+
