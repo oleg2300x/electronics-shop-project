2b9b397ae5219c8be231bb7e9f2034bfd9b0fc89
Oleg <panasenko23o@mail.ru>
2023-08-29 21:09:47 +1000

    add tests

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -8,7 +8,7 @@ from src.phone import Phone
 phone1 = Phone('Samsung', 20000, 7, 2)
 phone2 = Phone('iPhone', 80000, 10, 1)
 phone3 = Item('Huawey', 7000, 7)
-# phone4 = Phone('Oppo', 10100, 17, -2)
+
 
 
 def test_init():
@@ -17,9 +17,9 @@ def test_init():
     assert phone1.price == 20000
     assert phone1.quantity == 7
 
-# def test_phone_invalid_number_of_sim():
-#     with pytest.raises(ValueError):
-#         phone = Phone("Samsung", 599, 5, [2, 5, 6])
+def test_phone_invalid_number_of_sim():
+    with pytest.raises(ValueError):
+        phone = Phone("Samsung", 599, 5, [2, 5, 6])
 
 def test_add_different_types():
     with pytest.raises(TypeError):
